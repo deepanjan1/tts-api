2734187c7f09d7fd19b21f774e071ad7c30be522
deepanjan1 <deepanjan.de@gmail.com>
2019-04-28 20:26:32 -0400

    cleaned and stored text

--- server/load_articles.py
+++ server/load_articles.py
@@ -7,6 +7,14 @@ from config import POCKET_KEY, ACCESS_TOKEN
 import requests
 import json
 
+# URL encoding
+import urllib.parse
+
+# for parsing HTML
+from bs4 import BeautifulSoup
+import lxml
+import re
+
 # function that loads articles
 def loadArticles():
     '''function calls all articles from Pocket; this function is exported'''
@@ -18,28 +26,20 @@ def loadArticles():
     url = 'https://getpocket.com/v3/get'
 
     response = requests.post(url, data = parameters)
-    # response_escape = response.text.replace('\ ', '')
+
     articles_json_raw = json.loads(response.text)
 
     articles_json = []
     for key in articles_json_raw['list']:
         articleObject = articles_json_raw['list'][key]
-        # print (articleObject)
+
         if readableArticleValidator(articleObject):
-            # article_json = {
-            # 'title': articleObject['resolved_title'],
-            # 'percent': 0.2,
-            # 'key': key,
-            # 'image': articles_json_raw['list'][key]['top_image_url'],
-            # }
             article_json = articleObjectCreator(articleObject, key)
             articles_json.append(article_json)
 
-    # print (articles_json)
-    # print (articles_json_raw['list'])
     return (jsonify(articles_json))
 
-## Helper functions to not be exported ##
+####### Helper functions to not be exported #######
 
 def readableArticleValidator(articleObject):
     '''ensures the article has content to read'''
@@ -62,11 +62,39 @@ def articleObjectCreator(articleObject, key):
     else:
         image = requests.get('https://ui-avatars.com/api/', {'name': title}).url
 
+    articleText = retrieveText(articleObject['resolved_url'])
+    # print(articleText.get_text())
     article_json = {
     'title': title,
     'percent': 0.2,
     'key': key,
     'image': image,
+    'text': articleText
     }
 
     return article_json
+
+def retrieveText(url):
+    '''Retrieves text using Pocket's text parser API.
+    Inputs: URL, Outputs: object that includes the HTML text'''
+
+    apiURL = 'https://text.getpocket.com/v3/text'
+
+    # build data object with no images or videos
+    data = {
+    'consumer_key': POCKET_KEY,
+    'url': url,
+    'images': 0,
+    'videos': 0,
+    'output': 'json',
+    }
+
+    response = requests.post(apiURL, data=data)
+    response_json = json.loads(response.text)
+
+    # parse out text only from HTML
+    soup = BeautifulSoup(response_json['article'], 'lxml')
+    text_ascii = re.sub('(\n|\r)', ' ', soup.get_text())\
+    .replace('\u2019', "'")\
+    .replace('\"', '"')
+    return text_ascii
