69324547605a871243f5425203072c6febab5355
deepanjan1 <deepanjan.de@gmail.com>
2019-05-03 23:41:30 -0400

    created function that creates an audio file

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, jsonify, url_for
 from load_articles import loadArticles
+from tts import createAudioFile
 from config import POCKET_KEY, ACCESS_TOKEN
 
 # to read APIs
@@ -12,6 +13,11 @@ app = Flask(__name__)
 def getTracks():
     tracks = loadArticles()
     return (tracks)
+
+@app.route('/audio/mp3')
+def getAudio():
+    '''get track text, convert and store audio, output audio file'''
+    return (createAudioFile())
 
 if __name__ == '__main__':
     app.debug = True
